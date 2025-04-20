fd0f0813794a96ddf8d14a26fb2cd6c939d2b9c4
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-04-20 14:34:30 +0900

    Update:イコールボタンの実装

--- calc.js
+++ calc.js
@@ -1,9 +1,9 @@
 function num_clicked(btn){
     const display = document.querySelector('input[name="display"]');
     if (display.value === "0"){
-        display.value = btn.value
+        display.value = btn.value;
     }else{
-        display.value += btn.value
+        display.value += btn.value;
     }
 }
 
@@ -11,22 +11,29 @@ function dot_clicked(btn){
     const display = document.querySelector('input[name="display"]');
     const result = display.value.includes('.');
     if (!result){
-        display.value += btn.value
+        display.value += btn.value;
     }
 }
 
 function ac_clicked(){
     const display = document.querySelector('input[name="display"]');
-    display.value = "0"
+    display.value = "0";
 }
 
 function operation_clicked(btn){
     const display = document.querySelector('input[name="display"]');
-    const display_tail = display.value.slice(-1)
+    const display_tail = display.value.slice(-1);
     const operations = ['+', '-', '×', '÷', '%'];
     if(!operations.includes(display_tail)){
-        display.value += btn.value
+        display.value += btn.value;
     }else{
-        display.value = display.value.slice(0, -1) + btn.value
+        display.value = display.value.slice(0, -1) + btn.value;
     }
+}
+
+function equal_clicked(){
+    const display = document.querySelector('input[name="display"]');
+    const display_calc = display.value.replace(/×/g, '*').replace(/÷/g, '/');
+    const display_result = eval(display_calc);
+    display.value = display_result;
 }
